ede89f13ba53cde5a468adaf6bf3d9fa0bcfa7c0
stha008 <ujjwalshrestha008@gmail.com>
2023-12-20 19:54:39 +0545

    committing index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-// Include Composer's autoloader
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Use the Hello class from the Silarhi namespace
+
 use Silarhi\Hello;
 
-// Create an instance of the Hello class
+
 $hello = new Hello();
 
-// Call the display method and output its result
+
 echo $hello->display() . "\n";
 
